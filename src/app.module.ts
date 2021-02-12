9b5975f668c98a585a936fd98f63a865b3003b1f
Artyom Shymukovich <artyom.shymukovich@nindatech.com>
2021-02-12 15:18:24 +0800

    removed some trash

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ProductModule } from "./product/product.module";
-import { BarController } from './bar/bar.controller';
 
 @Module({
   imports: [ProductModule],
-  controllers: [AppController, BarController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
